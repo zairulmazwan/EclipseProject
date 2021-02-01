af33f2e88228a5aec8492bfc09a53e861c58e44e
Zairul Mazwan <zairulmazwan@gmail.com>
2021-02-01 16:52:31 +0000

    test

--- EclipseProjectGithub/src/HelloWorld.java
+++ EclipseProjectGithub/src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Test World!");
-
+		System.out.println("Hello my  ");
+        System.out.println("hello people");
+		
 	}
 
 }
